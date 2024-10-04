320c5c63c1e25b6492925b26117677bec89303cd
Ziad Hatem <ziadhatem2022@gmail.com>
2024-10-04 17:55:48 +0300

    fix

--- utils/db.ts
+++ utils/db.ts
@@ -4,9 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const databaseUrl = process.env.DATABASE_URL;
-console.log(`Database URL: ${databaseUrl}`);
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV == "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
