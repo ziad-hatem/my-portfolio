ec1fdb10983cadb41d5b54ef1cdf114cd7a7e4c5
Ziad Hatem <ziadhatem2022@gmail.com>
2024-10-04 04:40:23 +0300

    fix

--- utils/db.ts
+++ utils/db.ts
@@ -4,6 +4,21 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const databaseUrl = process.env.DATABASE_URL;
+console.log(`Database URL: ${databaseUrl}`);
+
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// Test the database connection
+async function testConnection() {
+  try {
+    await db.$connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+}
+
+testConnection();
